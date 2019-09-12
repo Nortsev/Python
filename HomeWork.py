fca9491813aea9ef3e3e2a0d68b54a00bc2ea26e
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2019-09-12 22:17:39 +0400

    # Создайте программу медицинская анкета, где вы запросите у пользователя такие данные, как имя, фамилию, возраст, и вес. # И выведите результат согласно которому пациент в хорошем состоянии, если ему до 30 лет и вес от 50 и до 120 кг, # Пациенту требуется начать вести правильный образ жизни, если ему более 30 и вес меньше 50 или больше 120 кг # Пациенту требуется врачебный осмотр, если ему более 40 и вес менее 50 или больше 120 кг. # Все остальные варианты вы можете обработать на ваш вкус и полет фантазии =) # Формула не отражает реальной действительности и здесь используется только ради примера.
    # Пример: Вася Пупкин, 31 год, вес 90 - хорошее состояние
    # Пример: Вася Пупкин, 31 год, вес 121 - следует заняться собой
    # Пример: Вася Пупкин, 31 год, вес 49 - следует заняться собой
    # Пример: Вася Пупкин, 41 год, вес 121 - следует обратится к врачу!
    # Пример: Вася Пупкин, 41 год, вес 49 - следует обратится к врачу!

---
+++ HomeWork.py
@@ -0,0 +1,14 @@
+﻿print('Добро пожаловать')
+name = input("Введите имя: ")
+surname = input('Введите фамилию: ')
+age = int(input('Введите возраст: '))
+weight = float(input('Введите свой вес: '))
+
+if age < 30 and weight in range(50, 121):
+    print(name, surname, ',вы в хорошем состоянии')
+elif age >= 30 and weight > 120:
+    print(name, surname, ',вам пора бы занятся собой ')
+elif age >= 40 and (weight < 50 or weight > 120):
+    print(name, surname, ' ,советуем обратится к врачу')
+else:
+    print('Программа окончена')
